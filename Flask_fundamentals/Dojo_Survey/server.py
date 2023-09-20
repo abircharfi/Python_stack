25a2a26d7ec25ddd6b9304372f6675790c36db87
Abir Charfi <abir8charfi@gmail.com>
2023-09-20 21:21:29 +0200

    second commit

--- Flask_fundamentals/Dojo_Survey/server.py
+++ Flask_fundamentals/Dojo_Survey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template ,request , session
+from flask import Flask , render_template ,request , session , redirect
 
 app = Flask(__name__)
 
@@ -7,17 +7,20 @@ app.secret_key ='sdqfcsdgrfvseg'
 @app.route('/')
 
 def index():
-    return render_template('index.html')
- 
 
-@app.route('/result', methods=['POST'])
+ return render_template('index.html')
 
-def result():
- name = request.form['name']
- location= request.form['location']
- language = request.form['language']
- comments= request.form['comments']
- return render_template('info.html', name=name, location=location, language=language, comments=comments)
+@app.route('/process', methods=['POST'])
+
+def create():
+   session['name'] = request.form['name']
+   session['location']= request.form['location']
+   session['language'] = request.form['language']
+   session['comments']= request.form['comments']
+   return redirect('result')
+@app.route('/result')
+def show():
+   return render_template('info.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
